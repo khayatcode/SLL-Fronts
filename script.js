0f97ab32ac02ebdd72d468dab65272a86cc32c79
khayatcode <a.khayatcode0103@gmail.com>
2023-06-22 01:55:34 -0600

    MaxMinAvg

--- script.js
+++ script.js
@@ -60,16 +60,85 @@ class SLL {
             return count
         }
     }
+    display(){
+        if(this.head == null){
+            return "No nodes in list"
+        }else{
+            let stringList = ""
+            let promise = this.head
+            while(promise){
+                if(promise.next == null){
+                    stringList += promise.data + ""
+                    return stringList
+                }
+                stringList += promise.data + ","
+                promise = promise.next
+            }
+            return stringList
+        }
+    }
+    max(){
+        if(this.head == null){
+            return "No nodes in list"
+        } else {
+            let promise = this.head
+            let maxNum = this.head.data
+            while(promise){
+                if(promise.data > maxNum){
+                    maxNum = promise.data
+                }
+                promise = promise.next
+            }
+            return maxNum
+        }
+    }
+    min(){
+        if(this.head == null){
+            return "No nodes in list"
+        } else {
+            let promise = this.head
+            let minNum = this.head.data
+            while(promise){
+                if(promise.data < minNum){
+                    minNum = promise.data
+                }
+                promise = promise.next
+            }
+            return minNum
+        }
+    }
+    average(){
+        if(this.head == null){
+            return "No nodes in list"
+        } else {
+            let promise = this.head
+            let length = 0
+            let total = 0
+            let average = 0
+            while(promise){
+                length++
+                total += promise.data
+                promise = promise.next
+            }
+            average = total/length
+            return average
+        }
+    }
 }
 
 let SLL1 = new SLL()
 console.log("addFront")
 console.log(SLL1)
-SLL1.addFront(18)
-console.log(SLL1)
-SLL1.addFront(20)
-console.log(SLL1)
+SLL1.addFront(19)
 SLL1.addFront(22)
 console.log(SLL1)
-console.log("length")
-console.log(SLL1.length())
+SLL1.addFront(34)
+SLL1.addFront(45)
+console.log(SLL1)
+SLL1.addFront(5)
+SLL1.addFront(6)
+SLL1.addFront(10)
+SLL1.addFront(20)
+console.log(SLL1)
+console.log("average")
+console.log(SLL1.average())
